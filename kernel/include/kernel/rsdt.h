3efd4b98fa9a29e31c10c95c2eaf87db1500e23f
belli49 <pedrokomessu@gmail.com>
2023-12-02 19:53:32 +0900

    Added RSDP checksum

--- kernel/include/kernel/rsdt.h
+++ kernel/include/kernel/rsdt.h
@@ -4,6 +4,8 @@
 typedef struct ACPISDTHeader_struct ACPISDTHeader;
 typedef struct RSDT_struct RSDT;
 typedef struct XSDT_struct XSDT;
+typedef struct RSDP_struct RSDP;
+typedef struct XSDP_struct XSDP;
 
 struct ACPISDTHeader_struct {
   char Signature[4];
@@ -29,15 +31,15 @@ struct XSDT_struct {
   uint64_t* PointerToOtherSDT;
 };
 
-struct RSDP_t {
+struct RSDP_struct {
  char Signature[8];
  uint8_t Checksum;
  char OEMID[6];
  uint8_t Revision;
  uint32_t RsdtAddress;
-} __attribute__ ((packed));
+};
 
-struct XSDP_t {
+struct XSDP_struct {
  char Signature[8];
  uint8_t Checksum;
  char OEMID[6];
@@ -48,8 +50,9 @@ struct XSDP_t {
  uint64_t XsdtAddress;
  uint8_t ExtendedChecksum;
  uint8_t reserved[3];
-} __attribute__ ((packed));
+};
 
 uintptr_t find_RSDT(void);
-bool doChecksum(ACPISDTHeader *tableHeader);
 void *findFACP(void *RootSDT);
+bool do_checksum_ACPISDT(ACPISDTHeader *tableHeader);
+bool do_checksum_RSDP(RSDP* rsdp);
