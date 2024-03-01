8f32307d0eb4a50705608b16b590bed973966d11
belli49 <pedrokomessu@gmail.com>
2024-03-02 05:56:43 +0900

    Finished setting up controllers; TODO: reset devices

--- kernel/arch/i386/devices/ps2.c
+++ kernel/arch/i386/devices/ps2.c
@@ -11,18 +11,23 @@
 #define PS2_COMMAND_REGISTER 0x64
 
 FADT* fadt;
+bool first_controller_works = false;
+bool second_controller_works = false;
 
 void init_fadt() {
   fadt = (FADT*) find_by_header("FACP");
 
+  // check is ps/2 controller available
   if (!rsdp->revision || fadt->BootArchitectureFlags & 2) {
     // if ACPI 1.0 just assume there is ps/2 controller
     // there is PS/2 controller
     printf("PS/2 available\n");
 
+
     // disable ports
     outb(PS2_COMMAND_REGISTER, 0xAD); // disable first IO port
     outb(PS2_COMMAND_REGISTER, 0xA7); // disable second IO port (ignored if single channel)
+
 
     // flush output buffer
     while (inb(PS2_STATUS_REGISTER) & 1) {
@@ -30,11 +35,13 @@ void init_fadt() {
       printf("flushing ps/2 out buffer\n");
     }
 
+
     // set controller configuration byte
     outb(PS2_COMMAND_REGISTER, 0x20);
     uint8_t controller_config = inb(PS2_DATA_PORT) & 0xBC;
     outb(PS2_COMMAND_REGISTER, 0x60);
     outb(PS2_DATA_PORT, controller_config);
+
 
     // test ps/2 controller
     outb(PS2_COMMAND_REGISTER, 0xAA);
@@ -48,8 +55,6 @@ void init_fadt() {
     }
 
 
-    // TODO: either sth is wrong or QEMU does not support a second channel
-    //       try to fix later
     // detect if dual channel and disable second channel if so
     bool is_dual_channel = controller_config & 0x20;
     if (is_dual_channel) {
@@ -60,18 +65,43 @@ void init_fadt() {
 
       // read controller configuration byte
       outb(PS2_COMMAND_REGISTER, 0x20);
-      uint8_t second_controller_config = inb(PS2_DATA_PORT);
-      printf("%x\n", second_controller_config);
-      is_dual_channel = second_controller_config & 0x20;
+      is_dual_channel = !(inb(PS2_DATA_PORT) & 0x20); // if bit is clear it is dual channel
 
       // disable second port
       if (is_dual_channel) {
-        printf("dual channel ps/2 detected\n");
+        printf("Dual channel ps/2 detected\n");
         outb(PS2_COMMAND_REGISTER, 0xA7);
       }
     }
 
   
+    // interface tests
+    // first port
+    outb(PS2_COMMAND_REGISTER, 0xAB);
+    first_controller_works = !inb(PS2_DATA_PORT);
+    if (!first_controller_works) printf("Interface test failed for port 1\n");
+    // second port
+    outb(PS2_COMMAND_REGISTER, 0xA9);
+    second_controller_works = !inb(PS2_DATA_PORT);
+    if (!second_controller_works) printf("Interface test failed for port 2\n");
+
+
+    // enable controllers
+    if (first_controller_works) outb(PS2_COMMAND_REGISTER, 0xAE);
+    if (second_controller_works) outb(PS2_COMMAND_REGISTER, 0xA8);
+
+
+    // enable interrupts
+    outb(PS2_COMMAND_REGISTER, 0x20);
+    controller_config = inb(PS2_DATA_PORT) | 3;
+    outb(PS2_COMMAND_REGISTER, 0x60);
+    outb(PS2_DATA_PORT, controller_config);
+
+
+
+    // TODO: reset devices
+
+
   } else {
     // no PS/2 controller
 
